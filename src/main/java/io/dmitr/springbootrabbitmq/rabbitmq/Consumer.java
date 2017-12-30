6455560af287e000eccde6e80281236f7c21a482
masdmtr <masdmtr@gmail.com>
2017-12-30 20:27:08 +0300

    project has been split onto Producer and Consumer

--- src/main/java/io/dmitr/springbootrabbitmq/rabbitmq/Consumer.java
+++ src/main/java/io/dmitr/springbootrabbitmq/rabbitmq/Consumer.java
@@ -1,4 +1,15 @@
 package io.dmitr.springbootrabbitmq.rabbitmq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
 public class Consumer {
+
+    private static final Logger log = LoggerFactory.getLogger(Consumer.class);
+
+    @RabbitListener(queues = "${queue-name}")
+    public void handler(String message) {
+        log.info("Consumer> " + message);
+    }
 }
